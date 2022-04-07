3e3325c68d1898deb302d17a5304c4df9790fbe8
Karin <karinivonne86@gmail.com>
2022-04-07 15:24:20 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,11 @@
-/*$(selector).click(function(e) {
-    e.preventDefault();
-
-});
-
-
-
-$(selector).toggle();*/
-
-
 $(function() {
 
     $("#enviarCorreo").click(function() {
         alert("El correo ha sido enviado correctamente...");
-    })
+    });
+
+
+    //   $("h4").on(css("text-color" = "red"), "dblclick")); <-- aqui esta el error
+
+
 });
